1fd4dd41ccf9bd2cfd55c312fe9f13bfeba5eb91
Daniel B Sherry <dsherry@proxlearn.com>
2023-09-01 16:55:27 -0500

    styling boii

--- app/controllers/billing_controller.rb
+++ app/controllers/billing_controller.rb
@@ -2,6 +2,6 @@ class BillingController < ApplicationController
     before_action :authenticate_user!
 
     def show
-			@portal_sesson = current_user.payment_processor.billing_portal
+		  @portal_sesson = current_user.payment_processor.billing_portal
     end
 end
